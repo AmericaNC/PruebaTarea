fd239e05e2ff6834e83ed37b9e042724a18ce1bd
AmericaNC <L22211621@tectijuana.edu.mx>
2023-03-17 20:44:26 -0700

    Actualizacion

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -1,48 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-
 namespace Pelicula
 {
-
     public class Pelicula
     {
         //Campos
-        private List<Actor> actores = new List<Actor>();
-
+        public string titulo;
+        public string pais;
+        public int año;
+        public string director;
+       // private List<Actor> actores = new List<Actor>();
         //Constructores
-     
         //Métodos
-     
+        public Pelicula(string t, string p, int a, string d){
+        this.titulo = t;
+        this.pais = p;
+        this.año = a;
+        this.director = d;
+        }
         public void Imprime()
         {
      //       Console.WriteLine($"{titulo} ({año})");
-
         }
-
-
     }
-
+/*
     public class Actor
     {
         //Propiedades
-
         //Constructores
-
-
         //Métodos 
         public void Imprime()
         {
             Console.WriteLine($"{Nombre} ({Año})");
         }
     }
-
-    // Puedes probar tu código en Main() pero lo importante
-    // es que pase las pruebas
+    // Puedes probar tu código en Main() pero lo importante es que pase las pruebas
+    / */ 
 
     class Program
     {
-
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
